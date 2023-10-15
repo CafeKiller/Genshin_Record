90476c8fb7f1e096042e5a8b1b77aae946b65e74
CoffeeKiller <18320210361@163.com>
2023-10-15 15:23:21 +0800

    remove cards model

--- src/route/index.ts
+++ src/route/index.ts
@@ -5,16 +5,12 @@ const Version = () => import("../pages/Version.vue")
 
 const RolePool = () => import("../pages/RolePool.vue")
 const WeaponPool = () => import("../pages/WeaponPool.vue")
-const RoleCrads = () => import("../pages/RoleCards.vue")
-const WeaponCrads = () => import("../pages/WeaponCards.vue")
 
 const routes: Array<RouteRecordRaw> = [
     { path: "/", name: "home", component: Home },
     { path: "/ver", name: "version", component: Version },
     { path: "/pool/role", name: "rolePool", component: RolePool },
-    { path: "/pool/weapon", name: "weaponPool", component: WeaponPool },
-    { path: "/cards/role", name: "roleCrads", component: RoleCrads },
-    { path: "/cards/weapon", name: "weaponCrads", component: WeaponCrads },
+    { path: "/pool/weapon", name: "weaponPool", component: WeaponPool }
 ]
 
 const router = createRouter({

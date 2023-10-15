cb5df3e5ae96f54d3288950227e629e271ca2469
CoffeeKiller <18320210361@163.com>
2023-10-15 15:28:00 +0800

    add mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
+import { viteMockServe } from "vite-plugin-mock"
 import vue from '@vitejs/plugin-vue'
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      viteMockServe({
+        mockPath:"./mock",  // mock文件所在位置
+        localEnabled:true,  // 是否应用于本地测试环境
+        prodEnabled: false, // 是否用于生产环境
+        supportTs: true,    // true 可以读取ts文件模块
+        watchFiles: true    // 监听mock文件修改
+      })
+  ],
   resolve:{
     alias:{
-      "@":path.resolve(__dirname,"./src") //配置@别名
+      "@":path.resolve(__dirname,"./src")   //配置@别名
     }
   },
 

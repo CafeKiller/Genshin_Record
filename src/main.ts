29432ae8ad54165ceb9f80d279937c42296782b4
CoffeeKiller <18320210361@163.com>
2023-10-15 12:48:13 +0800

    update 2023_10_15 12:48:11

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from '@/App.vue'
+import App from "@/App.vue"
 import router from "./route";
-console.log("%chello", "background:#09c;color: #ffffff;");
+console.log("%c hello", "background:#09c;color: #ffffff;");
 
 createApp(App).use(router).mount('#app')
